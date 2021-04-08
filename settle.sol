99caeba2865213c222fbd4ffdfd1444396ca5ca4
SightEyeFG <75752611+SightEyeFG@users.noreply.github.com>
2021-04-08 10:34:56 -0400

    Create settle.sol

---
+++ settle.sol
@@ -0,0 +1,141 @@
+pragma solidity ^0.5.0;
+
+contract Context {
+    constructor () internal {}
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+}
+
+
+pragma solidity ^0.5.0;
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+
+pragma solidity ^0.5.0;
+
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor () internal {
+        _owner = _msgSender();
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function isOwner() public view returns (bool) {
+        return _msgSender() == _owner;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+pragma solidity ^0.5.0;
+
+contract Governor is Context {
+    address private _guardian;
+
+    event GovernorTransferred(address indexed previousGuardian, address indexed newGuardian);
+
+    constructor () internal {
+        _guardian = _msgSender();
+        emit GovernorTransferred(address(0), _guardian);
+    }
+
+    function governor() public view returns (address) {
+        return _guardian;
+    }
+
+    modifier onlyGovernor() {
+        require(isGovernor(), "Caller is not the governor!");
+        _;
+    }
+
+    function isGovernor() public view returns (bool) {
+        return _msgSender() == _guardian;
+    }
+
+    function transferGovernor(address newGovernor) public onlyGovernor {
+        _transferGovernor(newGovernor);
+    }
+
+    function _transferGovernor(address newGovernor) internal {
+        require(newGovernor != address(0), "New governor is the zero address!");
+        emit GovernorTransferred(_guardian, newGovernor);
+        _guardian = newGovernor;
+    }
+}
+
+pragma solidity ^0.5.0;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
